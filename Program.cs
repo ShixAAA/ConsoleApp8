246f67cbba8be6c69bc3afb0749a02b55f06f15e
test05 <test5@user.com>
2021-10-19 11:57:16 +0300

    g

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 
-namespace ConsoleApp8
+namespace nomer1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double x, y, z, a, b, c;//Объявление переменных
-            Console.WriteLine("Введите x = ");
+            double x, y, z;//Объявление переменных
+            Console.WriteLine("Введите x = ");//Ввод и конвертирование переменной
             x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите y = ");
+            Console.WriteLine("Введите y = ");//Ввод и конвертирование переменной
             y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите z = ");
+            Console.WriteLine("Введите z = ");//Ввод и конвертирование переменной
             z = Convert.ToDouble(Console.ReadLine());
+            if (Math.Pow(z, 2) == Math.Pow(y, 2) + Math.Pow(x, 2))//Условие
+                Console.WriteLine("Треугольник прямоугольный");//Условие выполнено
+            if (Math.Pow(y, 2) == Math.Pow(z, 2) + Math.Pow(x, 2))
+                Console.WriteLine("Треугольник прямоугольный");//Условие выполнено
+            if (Math.Pow(x, 2) == Math.Pow(y, 2) + Math.Pow(z, 2))
+                Console.WriteLine("Треугольник прямоугольный");//Условие выполнено
+            else Console.WriteLine("Треугольник не прямоугольный");//Условие не выполнено
+            Console.ReadKey();
+
 
         }
     }
