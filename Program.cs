ea09749ba011b33aa58eb1c8dc5655c8d0508709
test05 <test5@user.com>
2021-10-19 11:01:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, a, b, c;//Объявление переменных
+            Console.WriteLine("Введите x = ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y = ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите z = ");
+            z = Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+}
